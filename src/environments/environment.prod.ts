de3660e3e95327bf50030eef9c118990271c68b4
Erdem Camlioglu <secamlioglu@gmail.com>
2021-03-07 00:55:44 +0300

    environmentsadjusted

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCDGfDlNvvzLlYz8Z0Rsta4BcmXXpFB77E",
+    authDomain: "for-words.firebaseapp.com",
+    projectId: "for-words",
+    storageBucket: "for-words.appspot.com",
+    messagingSenderId: "437113246401",
+    appId: "1:437113246401:web:89b500c52073d75f6f7a40",
+    measurementId: "G-7XMM2NSY4M"
+  }
 };
